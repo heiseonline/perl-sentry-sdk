9fdf06db44730ee1a7a54e7014d6b3d53b599188
pmb <pmb@heise.de>
2021-01-30 20:55:29 +0100

    wip Mojo::UserAgent

--- lib/Sentry/Integration/MojoUserAgent.pm
+++ lib/Sentry/Integration/MojoUserAgent.pm
@@ -5,6 +5,9 @@ use Mojo::Loader qw(load_class);
 use Mojo::Util qw(dumper monkey_patch);
 use Sub::Util 'set_subname';
 
+has breadcrumbs => 1;
+has tracing     => 1;
+
 my %Patched = ();
 
 sub around ($package, $method, $cb) {
@@ -23,20 +26,50 @@ sub around ($package, $method, $cb) {
   $Patched{$key} = 1;
 }
 
+sub measure ($cb, @args) {
+  use Time::HiRes 'time';
+  my $t0     = time;
+  my $result = $cb->(@args);
+  return {return_value => $result, duration => (time - $t0) * 1000};
+}
+
 sub setup_once ($self, $add_global_event_processor, $get_current_hub) {
+  return if (!$self->breadcrumbs && !$self->tracing);
+
+  around(
+    'Mojo::UserAgent',
+    start => sub ($orig, $ua, $tx, $cb) {
+      my $url = $tx->req->url;
+
+      # Exclude Requests to the Sentry server
+      return $orig->($ua, $tx, $cb)
+        if $tx->req->headers->header('x-sentry-auth');
+
+      my $result = measure($orig, $ua, $tx, $cb);
+
+      my $hub = $get_current_hub->();
+      $hub->add_breadcrumb({
+        type     => 'http',
+        category => 'xhr',
+        data     => {
+          url         => $tx->req->url->to_string,
+          method      => $tx->req->method,
+          status_code => $tx->res->code,
+          reason      => "OK"
+        }
+      });
+
+      # warn "### DURATION: " . $result->{duration} . "($url)";
+      # warn dumper($tx->req->headers);
+
+      return $result->{return_value};
+    }
+  );
+
   $add_global_event_processor->(
     sub ($event, $hint) {
-      warn 'PROCESS';
-      around(
-        'Mojo::UserAgent',
-        start => sub ($orig, @args) {
-          use Time::HiRes 'time';
-          my $t0     = time;
-          my $result = $orig->(@args);
-          warn '### ' . ((time - $t0) * 1000);
-          return $result;
-        }
-      );
+
+      # warn 'PROCESS' . dumper($event);
 
       # warn $get_current_hub->();
     }
