2067f34d4127f6a2bd45f801e943ce2879f84b6f
pmb <pmb@heise.de>
2021-02-11 09:42:43 +0100

    v1.0.1

--- lib/Sentry/SDK.pm
+++ lib/Sentry/SDK.pm
@@ -6,7 +6,7 @@ use Mojo::Util 'dumper';
 use Sentry::Client;
 use Sentry::Hub;
 
-our $VERSION = version->declare('v1.0.0');
+our $VERSION = version->declare('v1.0.1');
 
 sub _call_on_hub ($method, @args) {
   my $hub = Sentry::Hub->get_current_hub();
