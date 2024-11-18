b44cd54b4fea2ac49902f51cba008f2efae4b39b
pmb <pmb@heise.de>
2024-11-18 16:14:38 +0100

    1.3.8

--- lib/Sentry/SDK.pm
+++ lib/Sentry/SDK.pm
@@ -7,7 +7,7 @@ use Sentry::Client;
 use Sentry::Hub;
 use Sentry::Logger 'logger';
 
-our $VERSION = version->declare('1.3.7');
+our $VERSION = version->declare('1.3.8');
 
 sub _call_on_hub ($method, @args) {
   my $hub = Sentry::Hub->get_current_hub();
